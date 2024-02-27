ba646b6986ece68ccd738c4f0fd5d09b4aba5334
arbiosu <amea@oregonstate.edu>
2024-02-27 02:13:28 -0500

    updated database.py functions

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import sqlite3
 import datetime
+from models import User, Macro, Food
 
 
-def create_connection(db_file="calories.db") -> tuple[sqlite3.Connection,
-                                                      sqlite3.Cursor]:
+def create_connection(db_file="calories.db"):
     """
     Establishes a connection to the calories database.
 
@@ -11,8 +11,9 @@ def create_connection(db_file="calories.db") -> tuple[sqlite3.Connection,
         db_file (str): Path to the SQLite database file (default: "calories.db)
 
     Returns:
-        conn (sqlite3.Connection): A connection object
-        curr (sqlite3.Cursor): A cursor object for executing SQL queries
+        A tuple containing:
+            conn (sqlite3.Connection): A connection object
+            curr (sqlite3.Cursor): A cursor object for executing SQL queries
     """
 
     try:
@@ -24,7 +25,7 @@ def create_connection(db_file="calories.db") -> tuple[sqlite3.Connection,
         print(f"ERROR: could not establish connection to '{db_file}' ({e})")
 
 
-def create_users_table(conn: sqlite3.Connection, cur: sqlite3.Cursor) -> None:
+def create_users_table(conn, cur):
     """
     Creates the users table.
 
@@ -55,7 +56,7 @@ def create_users_table(conn: sqlite3.Connection, cur: sqlite3.Cursor) -> None:
         conn.close()
 
 
-def create_macros_table(conn: sqlite3.Connection, cur: sqlite3.Cursor) -> None:
+def create_macros_table(conn, cur):
     """
     Creates the macros table.
 
@@ -90,7 +91,7 @@ def create_macros_table(conn: sqlite3.Connection, cur: sqlite3.Cursor) -> None:
         conn.close()
 
 
-def create_usermacros_table(conn: sqlite3.Connection, cur: sqlite3.Cursor) ->None:
+def create_usermacros_table(conn, cur):
     """
     Creates the usermacros intersection table.
 
@@ -117,7 +118,7 @@ def create_usermacros_table(conn: sqlite3.Connection, cur: sqlite3.Cursor) ->Non
         conn.close()
 
 
-def create_foods_table(conn: sqlite3.Connection, cur: sqlite3.Cursor) ->None:
+def create_foods_table(conn, cur):
     """
     Creates the foods table.
 
@@ -152,7 +153,7 @@ def create_foods_table(conn: sqlite3.Connection, cur: sqlite3.Cursor) ->None:
         conn.close()
 
 
-def create_foodentries_table(conn: sqlite3.Connection, cur: sqlite3.Cursor) ->None:
+def create_foodentries_table(conn, cur):
     """
     Creates the foodentries intersection table.
 
@@ -184,3 +185,172 @@ def create_foodentries_table(conn: sqlite3.Connection, cur: sqlite3.Cursor) ->No
         print(f"ERROR: could not create foodentries table! ({e})")
     finally:
         conn.close()
+
+
+def create_user(user: User, conn, cur):
+    """
+    Create a user.
+
+    Args:
+        user (User): a User object
+        conn (sqlite3.Connection): A connection object
+        curr (sqlite3.Cursor): A cursor object for executing SQL queries
+    """
+
+    sql = "INSERT INTO users(username, weight, weightGoal) VALUES (?, ?, ?)"
+    try:
+        cur.execute(sql, (user.username, user.weight, user.weight_goal))
+        conn.commit()
+        print(f"Welcome, {user.username}.")
+    except sqlite3.Error as e:
+        print(f"ERROR: could not create user! ({e})")
+    finally:
+        conn.close()
+
+
+def select_specific_user(username: str, conn, cur) -> str:
+    """
+    Select a specific user by their username.
+    Args:
+        user (User): a User object
+        conn (sqlite3.Connection): A connection object
+        curr (sqlite3.Cursor): A cursor object for executing SQL queries
+
+    Returns:
+        user_id: the id of the user specified
+    """
+    sql = "SELECT id FROM users WHERE username=?"
+    try:
+        cur.execute(sql, (username))
+        user_id = cur.fetchone()[0]
+        return user_id
+    except sqlite3.Error as e:
+        print(f"Could not fetch the id of the user: {username}! ({e})")
+
+
+def create_macro(username: str, macro: Macro, conn, cur):
+    """
+    Creates a macro for a specified user and records it into usermacros.
+
+    Args:
+        username: the username of the user creating a macro.
+        macro: a Macro object.
+        conn (sqlite3.Connection): A connection object
+        curr (sqlite3.Cursor): A cursor object for executing SQL queries
+    """
+    user_id = select_specific_user(username, conn, cur)
+
+    sql = """
+    INSERT INTO macros(id, name, proteinAllocation, fatAllocation,
+    carbAllocation, calGoal) VALUES (?, ?, ?, ?, ?, ?)
+    """
+    usermacros_sql = "INSERT INTO usermacros(userID, macroID) VALUES (?, ?)"
+    try:
+        cur.execute(sql, (None, macro.name, macro.protein_pct, macro.fat_pct,
+                          macro.carb_pct, macro.cal_goal))
+        conn.commit()
+        macro_id = cur.lastrowid
+        cur.execute(usermacros_sql, user_id, macro_id)
+        print(f"Recorded {macro.name} for user {username}")
+    except sqlite3.Error as e:
+        print(f"ERROR: Could not record macro {macro.name}! ({e})")
+    finally:
+        conn.close()
+
+
+def create_food(food: Food, conn, cur):
+    """
+    Creates a food item.
+
+    Args:
+        food: a Food object.
+        conn (sqlite3.Connection): A connection object
+        curr (sqlite3.Cursor): A cursor object for executing SQL queries
+    """
+
+    sql = """
+    INSERT INTO foods(id, name, calories, protein, fat, carbs)
+    VALUES (?, ?, ?, ?, ?, ?)
+    """
+
+    try:
+        cur.execute(sql, (None, food.name, food.calories, food.protein,
+                          food.fat, food.carbs))
+        conn.commit()
+        print(f"Successfully logged {food.name} into the database.")
+    except sqlite3.Error as e:
+        print(f"Could not log {food.name} into the database! ({e})")
+    finally:
+        conn.close()
+
+
+def select_food_item(food_name: str, conn, cur) -> str:
+    """
+    Gets the id of the food item.
+
+    Args:
+        food_name: a string representing the name of the food item.
+        conn (sqlite3.Connection): A connection object.
+        curr (sqlite3.Cursor): A cursor object for executing SQL queries.
+
+    Returns:
+        food_id: the id of the food specified.
+    """
+
+    sql = "SELECT id FROM foods WHERE name=?"
+
+    try:
+        cur.execute(sql, (food_name))
+        food_id = cur.fetchone()[0]
+        return food_id
+    except sqlite3.Error as e:
+        print(f"Could not fetch the id of {food_name}! ({e})")
+
+
+def create_entry(username: str, food_name: str, conn, cur):
+    """
+    Creates a foodentry for a user, indicating they consumed this food on the
+    given date. Date defaults to current day and time.
+
+    Args:
+        username: a string representing the user who is logging the entry.
+        food_name: a string representing the name of the food consumed.
+        conn (sqlite3.Connection): A connection object.
+        curr (sqlite3.Cursor): A cursor object for executing SQL queries.
+    """
+
+    food_id = select_specific_user(username, conn, cur)
+    user_id = select_food_item(food_name, conn, cur)
+    sql = "INSERT INTO foodentries(foodID, userID) VALUES(?, ?)"
+
+    try:
+        cur.execute(sql, (food_id, user_id))
+        conn.commit()
+        print(f"Successfully recorded this entry for user {username}")
+    except sqlite3.Error as e:
+        print(f"Could not log this entry! ({e})")
+    finally:
+        conn.close()
+
+
+def show_current_entry(username: str, conn, cur):
+    """
+    Shows all entries for today.
+
+    Args:
+        username: a string representing the user
+        conn (sqlite3.Connection): A connection object.
+        curr (sqlite3.Cursor): A cursor object for executing SQL queries.
+    """
+
+    today = datetime.date.today()
+    user_id = select_specific_user(username, conn, cur)
+    sql = "SELECT * FROM foodentries WHERE userID=? AND date=?"
+
+    try:
+        cur.execute(sql, (user_id, today))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except sqlite3.Error as e:
+        print(f"Could not fetch entries for {username} on {today}! ({e})")
